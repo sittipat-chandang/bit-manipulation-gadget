c8cab57e1cf667de2cfa1e6cd9beb44dc50f0a62
ISAC\sitti <sittipat2554aa@hotmail.com>
2024-04-20 12:52:39 +0700

    Added page indicator constructor.

--- app/src/main/java/org/classup/bitmanipulationgadget/Utilities.kt
+++ app/src/main/java/org/classup/bitmanipulationgadget/Utilities.kt
@@ -1,5 +1,23 @@
 package org.classup.bitmanipulationgadget
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import org.classup.bitmanipulationgadget.ui.theme.BMGPageIndicator
+import org.classup.bitmanipulationgadget.ui.theme.BMGText
+
 const val INVALID_TEXT = "Invalid input!"
 
 fun inputTo64Binary(input: String): String {
@@ -55,4 +73,31 @@ fun spaceEvery4th(string: String): String {
 fun padBinary16Divisible(binaryString: String): String {
     val padding = if (binaryString.length % 16 > 0) 16 - binaryString.length % 16 else 0
     return "0".repeat(padding) + binaryString
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PageIndicator (pages: Int, pagerState: PagerState) {
+    if (pages < 2) { return }
+
+    Row(
+        Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        horizontalArrangement = Arrangement.Center
+    )
+    {
+        repeat(pagerState.pageCount) { iteration ->
+            val color =
+                if (pagerState.currentPage == iteration) BMGText else BMGPageIndicator
+            Box(
+                modifier = Modifier
+                    .padding(2.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(12.dp)
+            )
+        }
+    }
 }
